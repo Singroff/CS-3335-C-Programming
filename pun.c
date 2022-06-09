6b7dfb4e44ae8ce02e273285b9c2a1fd86b17a38
Singroff <caleb.epps@yahoo.com>
2022-06-09 14:25:03 -0400

    - Second Exercise

--- pun.c
+++ pun.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
-int main() {
-    printf("To c or not to c; That is the question.\n");
-    return 0;
-}
+//int main() {
+//    printf("To c or not to c; That is the question.\n");
+//    return 0;
+//}
 
